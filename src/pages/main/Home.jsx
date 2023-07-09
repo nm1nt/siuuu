6937b2a91b63818458af8f265001cb5c030e8aba
slime-123 <thanhdepzai110507@gmail.com>
2023-07-09 11:26:48 +0700

    home page change

--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -95,7 +95,7 @@ const Home = () => {
 
 
   return (
-    <div className="h-full w-full bg-blue-200 flex flex-1 justify-center">
+    <div className="h-full w-full  flex flex-1 justify-center">
       <section className="bg-white">
         <div className="w-full px-5 py-6 mx-auto space-y-5 sm:py-8 md:py-12 sm:space-y-8 md:space-y-16 max-w-7xl">
           <div className="flex flex-col items-center sm:px-5 md:flex-row">
@@ -110,7 +110,7 @@ const Home = () => {
                   <svg className="w-3.5 h-3.5 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                   <span>Featured</span>
                 </div>
-                <h1 className="text-4xl font-bold leading-none lg:text-5xl xl:text-6xl"><a href="#_">Savory Templates. Sweet Designs.</a></h1>
+                <h1 className="text-4xl font-bold leading-none lg:text-5xl xl:text-6xl "><a className="text-black" href="#_">Savory Templates. Sweet Designs.</a></h1>
                 <p className="pt-2 text-sm font-medium">by <a href="#_" className="mr-1 underline">John Doe</a> · <span className="mx-1">April 23rd, 2021</span> · <span className="mx-1 text-gray-600">5 min. read</span></p>
               </div>
             </div>
@@ -124,7 +124,7 @@ const Home = () => {
               <div className="bg-purple-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white inline-block">
                 <span>Lifestyle</span>
               </div>
-              <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a href="#_">Creating a Future Worth Living</a></h2>
+              <h2 className="text-lg font-bold sm:text-xl md:text-2xl"><a className="text-black" href="#_">Creating a Future Worth Living</a></h2>
               <p className="text-sm text-gray-500">Learn the attributes you need to gain in order to build a future and create a life that you are truly happy with.</p>
               <p className="pt-2 text-xs font-medium"><a href="#_" className="mr-1 underline">Mary Jane</a> · <span className="mx-1">April 17, 2021</span> · <span className="mx-1 text-gray-600">3 min. read</span></p>
             </div>
